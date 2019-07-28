3cc9b9f242116e98aeb94ee22019e18729310fc7
Michael Schneider <michaelschneider@Michaels-MBP.PK5001Z>
2019-07-27 22:58:40 -0600

    Add error checking and data model refinement

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var todoListArray = [];
 var navBody = document.querySelector("nav");
 var titleInput = document.querySelector(".nav__input--title");
 var listInput = document.querySelector(".nav__input--task-item");
+var titleInputError = document.querySelector("#nav__div--title-error");
+var listInputError = document.querySelector("#nav__div--list-error");
 
 navBody.addEventListener('click', navEventHandler);
 
@@ -17,16 +19,26 @@ function navEventHandler(e) {
 }
 
 function createNewTask(taskString, e) {
-  var taskItem = {
-    text: taskString,
-    isComplete: false,
-  }
-  var taskli = new TaskList(taskItem);
-  todoListArray[0].updateTask(taskli);
-  console.log("todoListArray====",todoListArray);
-}
+  if (checkInputs() === false){
+    return;
+  } else {
+    var taskItem = {
+      text: taskString,
+      isComplete: false,
+    }
+    var taskli = new TaskList(taskItem);
+    //for loop?? that adds stuff to our tasklist??
+    //a method in our class TaskList?!  FUCK YA!!
+    //for (var i = 0; i < )
+    todoListArray[0].updateTask(taskli);
+    console.log("todoListArray====",todoListArray);
+    };
+};
 
 function createNewCard(titleString) {
+  if (checkInputs() === false){
+    return;
+  }
   var todoObject = {
     id: Date.now(),
     title: titleString,
@@ -38,3 +50,35 @@ function createNewCard(titleString) {
   todoListArray.push(todolist);
   console.log("Global Array===", todoListArray);
 }
+
+function checkInputs (){
+  if ((checkTitleInput() === true) && (checkListInput() === true)){
+    console.log("checkTitleInput and checkLoist Input are returning true");
+    titleInputError.classList.add("hide");
+    listInputError.classList.add("hide");
+    return true;
+  } else if ((checkTitleInput() === true) && (checkListInput() === false)){
+    titleInputError.classList.add("hide");
+    listInputError.classList.remove("hide"); 
+    return false;
+  } else {
+    titleInputError();
+    return false;
+  }
+}
+
+function checkTitleInput(){
+  if (titleInput.value !== ""){
+    return true;
+  }
+  titleInputError.classList.remove("hide");
+  return false;
+}
+
+function checkListInput(){
+  if (listInput.value !== ""){
+    return true;
+  }
+  listInputError.classList.remove("hide");
+  return false;
+};
