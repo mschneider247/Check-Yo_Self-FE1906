172503c08fd5980ac39209c2a82dab26d5bc0b4c
Michael Schneider <michaelschneider@Michaels-MacBook-Pro.local>
2019-07-28 14:45:18 -0600

    Data model refinements, todoObjects and taskObjects populating correctly

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var todoListArray = [];
+var todoGlobalArray = [];
 var navBody = document.querySelector("nav");
 var titleInput = document.querySelector(".nav__input--title");
 var listInput = document.querySelector(".nav__input--task-item");
@@ -6,54 +6,82 @@ var titleInputError = document.querySelector("#nav__div--title-error");
 var listInputError = document.querySelector("#nav__div--list-error");
 
 navBody.addEventListener('click', navEventHandler);
+titleInput.addEventListener('mouseout', newObjectTitle);
 
 function navEventHandler(e) {
   console.log(e);
   if (e.target.id === 'nav__button--add-new-task') {
+    console.log("createNewCard firing!!!");
     createNewCard(titleInput.value);
   }
   if (e.target.className === 'nav__button--plus') {
     console.log("listInput.value===", listInput.value);
-    createNewTask(listInput.value)
+    console.log("createNewCard firing!!!");
+    createNewTask(listInput.value, e);
   }
+}
+
+function newObjectTitle(e){
+  console.log("mouseout is firing!");
+  if (checkTitleInput() === false){
+    return;
+  } else {
+    for (var i = 0; i < todoGlobalArray.length; i++){
+      if (todoGlobalArray[i].title === titleInput.value){
+        return;
+      };
+    };
+  }
+  createNewCard(titleInput.value); 
 }
 
 function createNewTask(taskString, e) {
   if (checkInputs() === false){
     return;
   } else {
-    var taskItem = {
+    var taskObject = {
       text: taskString,
       isComplete: false,
     }
-    var taskli = new TaskList(taskItem);
-    //for loop?? that adds stuff to our tasklist??
-    //a method in our class TaskList?!  FUCK YA!!
-    //for (var i = 0; i < )
-    todoListArray[0].updateTask(taskli);
-    console.log("todoListArray====",todoListArray);
+    var taskli = new TaskList(taskObject);
+    for (var i = 0; i < todoGlobalArray.length; i ++){
+      if (todoGlobalArray[i].title === titleInput.value){
+        todoGlobalArray[i].updateTask(taskli);
+        // function to populate the DOM and clear the input
+        populateTaskInDOM(taskli);
+      }
+    }
+    console.log("todoListArray====",todoGlobalArray);
     };
 };
 
+function populateTaskInDOM(taskli) {
+  console.log("populateTaskInDOM is firing!!");
+  console.log("taskli object is====", taskli);
+}
+
 function createNewCard(titleString) {
-  if (checkInputs() === false){
-    return;
-  }
+  // if (checkInputs() === false){
+  //   console.log("checkInputs() is returning false!!!");
+  //   return;
+  // }
   var todoObject = {
     id: Date.now(),
     title: titleString,
     urgent: false,
     tasks: [],
   }
-  var todolist = new ToDoList(todoObject);
-  console.log(todolist);
-  todoListArray.push(todolist);
-  console.log("Global Array===", todoListArray);
+  var todoItem = new ToDoList(todoObject);
+  // call a method?
+  // todolist.addTaskList(listInput.value);
+  // console.log(todolist);
+  todoGlobalArray.push(todoItem);
+  console.log("Global Array===", todoGlobalArray);
 }
 
 function checkInputs (){
   if ((checkTitleInput() === true) && (checkListInput() === true)){
-    console.log("checkTitleInput and checkLoist Input are returning true");
+    console.log("checkTitleInput and checkList Input are returning true");
     titleInputError.classList.add("hide");
     listInputError.classList.add("hide");
     return true;
@@ -69,6 +97,7 @@ function checkInputs (){
 
 function checkTitleInput(){
   if (titleInput.value !== ""){
+    titleInputError.classList.add("hide");
     return true;
   }
   titleInputError.classList.remove("hide");
